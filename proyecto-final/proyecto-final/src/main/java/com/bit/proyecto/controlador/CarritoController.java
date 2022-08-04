4d0d6f54f383769c6927639c0d9137ba9f688349
BorisTorres <boris96bs7@gmail.com>
2022-08-04 00:07:21 -0500

    Respaldo anterior sin JWT

--- proyecto-final/proyecto-final/src/main/java/com/bit/proyecto/controlador/CarritoController.java
+++ proyecto-final/proyecto-final/src/main/java/com/bit/proyecto/controlador/CarritoController.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-<<<<<<< HEAD
-=======
+
 import org.springframework.web.bind.annotation.DeleteMapping;
->>>>>>> RespladoSinJWT
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
